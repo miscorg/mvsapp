67b1e0fb450d97b9161f64dc449db220fa2057a1
Arun Peter <arunpeter@gmail.com>
2019-03-17 15:46:56 +0530

    Auto Populate

--- src/main/java/com/sbi/mvs/repository/PfhrmsRepository.java
+++ src/main/java/com/sbi/mvs/repository/PfhrmsRepository.java
@@ -4,7 +4,10 @@ import com.sbi.mvs.entity.Pfhrms;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PfhrmsRepository extends JpaRepository<Pfhrms,String> {
 
+    List<Pfhrms> findAllByPfIdLike(String userId);
 }
